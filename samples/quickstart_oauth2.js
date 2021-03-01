649f212b4bbe2708f3110cea1ec2fb7464baab53
Sofia Leon <sofialeon@google.com>
2021-03-01 15:37:33 -0800

    fix: samples

--- samples/quickstart_oauth2.js
+++ samples/quickstart_oauth2.js
@@ -158,7 +158,7 @@ function main(propertyId = 'YOUR-GA4-PROPERTY-ID') {
 
     const [response] = await analyticsDataClient.runReport({
       entity: {
-        propertyId: 'properties/' + propertyId,
+        propertyId: `properties/${propertyId}`,
       },
       dateRanges: [
         {
