f84a650a193b7bc318dbc94ffb5538cd0227ccb9
Anwesha <anweshanaskar@gmail.com>
2022-05-02 11:30:02 -0700

    Modifies strings to formatted strings for consistency

--- samples/runReportWithMultipleDimensionFilters.js
+++ samples/runReportWithMultipleDimensionFilters.js
@@ -98,17 +98,13 @@ function main(propertyId = 'YOUR-GA4-PROPERTY-ID') {
   // Prints results of a runReport call.
   function printRunReportResponse(response) {
     //[START analyticsdata_print_run_report_response_header]
-    console.log(response.rowCount + ' rows received');
+    console.log(`${response.rowCount} rows received`);
     response.dimensionHeaders.forEach(dimensionHeader => {
-      console.log('Dimension header name: ' + dimensionHeader.name);
+      console.log(`Dimension header name: ${dimensionHeader.name}`);
     });
     response.metricHeaders.forEach(metricHeader => {
       console.log(
-        'Metric header name: ' +
-          metricHeader.name +
-          ' (' +
-          metricHeader.type +
-          ')'
+        `Metric header name: ${metricHeader.name} (${metricHeader.type})`
       );
     });
     //[END analyticsdata_print_run_report_response_header]
@@ -116,7 +112,9 @@ function main(propertyId = 'YOUR-GA4-PROPERTY-ID') {
     // [START analyticsdata_print_run_report_response_rows]
     console.log('Report result:');
     response.rows.forEach(row => {
-      console.log(row.dimensionValues[0].value, ',', row.metricValues[0].value);
+      console.log(
+        `${row.dimensionValues[0].value}, ${row.metricValues[0].value}`
+      );
     });
     // [END analyticsdata_print_run_report_response_rows]
   }
